e91c0e497054dfb7898225e852fee7fe9c56deb1
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2019-01-15 20:45:08 +0100

    Align code

--- taskwarrior.el
+++ taskwarrior.el
@@ -225,10 +225,10 @@ the front and focus it.  Otherwise, create one and load the data."
   "Turn the taskwarrior timestamp into a readable format"
   (if (not ts)
       ts
-    (let ((year (substring ts 0 4))
-	  (month (substring ts 4 6))
-	  (day (substring ts 6 8))
-	  (hour (substring ts 9 11))
+    (let ((year   (substring ts 0 4))
+	  (month  (substring ts 4 6))
+	  (day    (substring ts 6 8))
+	  (hour   (substring ts 9 11))
 	  (minute (substring ts 11 13))
   	  (second (substring ts 13 15)))
       (format "%s-%s-%s %s:%s:%s" year month day hour minute second))))
